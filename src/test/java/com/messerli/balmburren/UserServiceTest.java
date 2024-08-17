51fb5c2bc13c4765c2762a5b82bf87a4f164a4c7
Bernhard Messerli <bernhard.messerli.5@gmail.com>
2024-08-17 13:34:02 +0200

    17082024/1333

--- src/test/java/com/messerli/balmburren/UserServiceTest.java
+++ src/test/java/com/messerli/balmburren/UserServiceTest.java
@@ -17,7 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
-
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
@@ -30,7 +30,9 @@ import static org.junit.Assert.assertEquals;
 public class UserServiceTest {
     @Autowired
     private UserService userService;
+    @Autowired
     private TourService tourService;
+    @Autowired
     private DatesService datesService;
     private AuthenticationService authenticationService;
     private RegisterUserDto userDto;
@@ -71,38 +73,37 @@ public class UserServiceTest {
         user1 = userRepository.save(user1);
     }
     @Test
-    public void persistUser() {
-        
+    public void checkUserRole() {
 
         Optional<User> user = userService.findUser("admin");
 
         assertEquals(roleRepository.findByName(RoleEnum.ADMIN), user.get().getRoles().stream().filter(e -> e.getName().equals(RoleEnum.ADMIN)).findFirst());
-       
 
+    }
+    @Test
+    public void checkTour() {
 
-
-//        Optional<Tour> tour = Optional.of(new Tour());
-//        tour.get().setNumber("1");
-//        tour = tourService.saveTour(tour.get());
-//        Optional<Dates> dates1 = Optional.of(new Dates());
-//            Optional<Dates> dates2 = Optional.of(new Dates());
-//        dates2.get().setDate("2023-05-09T00:00:00.000Z");
-//        dates1.get().setDate(new Date().toString());
-//        dates1 = datesService.saveDate(dates1.get());
-//        dates2 = datesService.saveDate(dates2.get());
-//        Optional<TourBindDates> tourBindDates = Optional.of(new TourBindDates());
-//        Optional<TourBindDates> tourBindDates1 = Optional.of(new TourBindDates());
-//        tourBindDates.get().setDates(dates1.get());
-//        tourBindDates.get().setTour(tour.get());
-//        tourBindDates1.get().setDates(dates2.get());
-//        tourBindDates1.get().setTour(tour.get());
-//        tourBindDates = tourService.saveTourBindDates(tourBindDates.get());
-//        tourBindDates1 = tourService.saveTourBindDates(tourBindDates1.get());
-////        assertEquals(tourService.getAllTourBindDatesForTour(tour).get(0), tourBindDates);
-//        assertEquals(tourService.getTourBindDates(tour.get(), dates1.get()), tourBindDates);
-//        tourService.deleteTourBindDates(tour.get(), dates1.get());
-//        assertEquals(tourService.getAllTourBindDatesForTour(tour.get()).isPresent(), true);
-//        assertEquals(tourService.getTourBindDates(tour.get(), dates2.get()), tourBindDates1);
+        Optional<Tour> tour = Optional.of(new Tour());
+        tour.get().setNumber("1");
+        tour = tourService.saveTour(tour.get());
+        Optional<Dates> dates1 = Optional.of(new Dates());
+        Optional<Dates> dates2 = Optional.of(new Dates());
+        dates2.get().setDate("2023-05-09T00:00:00.000Z");
+        dates1.get().setDate(new Date().toString());
+        dates1 = datesService.saveDate(dates1.get());
+        dates2 = datesService.saveDate(dates2.get());
+        Optional<TourBindDates> tourBindDates = Optional.of(new TourBindDates());
+        Optional<TourBindDates> tourBindDates1 = Optional.of(new TourBindDates());
+        tourBindDates.get().setDates(dates1.get());
+        tourBindDates.get().setTour(tour.get());
+        tourBindDates1.get().setDates(dates2.get());
+        tourBindDates1.get().setTour(tour.get());
+        tourBindDates = tourService.saveTourBindDates(tourBindDates.get());
+        tourBindDates1 = tourService.saveTourBindDates(tourBindDates1.get());
+        assertEquals(tourService.getTourBindDates(tour.get(), dates1.get()), tourBindDates);
+        tourService.deleteTourBindDates(tour.get(), dates1.get());
+        assertEquals(tourService.getAllTourBindDatesForTour(tour.get()).isPresent(), true);
+        assertEquals(tourService.getTourBindDates(tour.get(), dates2.get()), tourBindDates1);
     }
 
 }
