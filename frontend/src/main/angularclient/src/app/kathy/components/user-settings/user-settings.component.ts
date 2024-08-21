052abdb2b0fdb11c422d548c6a68da03f5b91b87
Bernhard Messerli <bernhard.messerli.5@gmail.com>
2024-08-21 10:16:15 +0200

    21082024/1016

--- frontend/src/main/angularclient/src/app/kathy/components/user-settings/user-settings.component.ts
+++ frontend/src/main/angularclient/src/app/kathy/components/user-settings/user-settings.component.ts
@@ -11,7 +11,56 @@ import {UserBindRoleDTO} from "../../../components/user/service/userBindRoleDTO"
   templateUrl: './user-settings.component.html',
   styleUrls: ['./user-settings.component.css']
 })
-export class UserSettingsComponent implements OnInit  {
+export class UserSettingsComponent implements OnInit {
+  // users: UserDTO[] | undefined;
+  // // usersWithRole: UserWithRoleDTO[] = [];
+  //
+  // constructor(private userService: UserService,
+  //             private router: Router) {
+  // }
+  //
+  // async ngOnInit(){
+  //   this.users = await firstValueFrom(this.userService.findAll());
+  //   if(this.users) {
+  //     this.users = this.users.sort((u1: UserDTO, u2: UserDTO) => {
+  //       if (u1.lastname && u2.lastname) return u1.lastname.localeCompare(u2.lastname)
+  //       else return 0;
+  //     });
+  //     for (const user of this.users) {
+  //       let userWithRole: UserWithRoleDTO = new UserWithRoleDTO();
+  //       let userBindRoles: UserBindRoleDTO[] = await firstValueFrom(this.userService.findAllRolesForPerson(user.username));
+  //       userBindRoles.forEach(userBindRole => {
+  //         if (userBindRole.role.name === "ROLE_SUPER_ADMIN") userWithRole.adminIsChecked = true;
+  //         if (userBindRole.role.name === "ROLE_DRIVER") userWithRole.driverIsChecked = true;
+  //         if (userBindRole.role.name === "ROLE_USER") userWithRole.userIsChecked = true;
+  //       });
+  //       userWithRole.username = user.username;
+  //       userWithRole.firstname = user.firstname;
+  //       userWithRole.lastname = user.lastname;
+  //       this.usersWithRole.push(userWithRole);
+  //     }
+  //   }
+  // }
+  //
+  // async userOrder(user: UserWithRoleDTO) {
+  //   let bool = await firstValueFrom(this.userService.isUserKathy(user.username));
+  //   if (bool)
+  //     await this.router.navigate(['kathy_users_order/'],
+  //     {
+  //       queryParams: {
+  //         param1: user.username
+  //       }
+  //     });
+  // }
+  //
+  // userOrdered(user: UserWithRoleDTO) {
+  //   this.router.navigate(['kathy_users_ordered/'],
+  //     {
+  //       queryParams: {
+  //         param1: user.username
+  //       }
+  //     });
+  // }
   users: UserDTO[] | undefined;
   usersWithRole: UserWithRoleDTO[] = [];
 
@@ -19,20 +68,29 @@ export class UserSettingsComponent implements OnInit  {
               private router: Router) {
   }
 
-  async ngOnInit(){
+  async ngOnInit() {
     this.users = await firstValueFrom(this.userService.findAll());
-    if(this.users) {
+    if (this.users) {
       this.users = this.users.sort((u1: UserDTO, u2: UserDTO) => {
         if (u1.lastname && u2.lastname) return u1.lastname.localeCompare(u2.lastname)
         else return 0;
       });
       for (const user of this.users) {
+        let user1: UserDTO = await firstValueFrom(this.userService.findUser(user.username));
         let userWithRole: UserWithRoleDTO = new UserWithRoleDTO();
-        let userBindRoles: UserBindRoleDTO[] = await firstValueFrom(this.userService.findAllRolesForPerson(user.username));
+        // let userBindRoles: UserBindRoleDTO[] = await firstValueFrom(this.userService.findAllRolesForPerson(user.username));
+        let userBindRoles: UserBindRoleDTO[] = [];
+        user1.roles.forEach(e => {
+          let userBindRole: UserBindRoleDTO = new UserBindRoleDTO();
+          userBindRole.role = e;
+          userBindRole.person = user1;
+          userBindRoles.push(userBindRole);
+        })
+
         userBindRoles.forEach(userBindRole => {
-          if (userBindRole.role.name === "ROLE_SUPER_ADMIN") userWithRole.adminIsChecked = true;
-          if (userBindRole.role.name === "ROLE_DRIVER") userWithRole.driverIsChecked = true;
-          if (userBindRole.role.name === "ROLE_USER") userWithRole.userIsChecked = true;
+          if (userBindRole.role.name == "ADMIN") userWithRole.adminIsChecked = true;
+          if (userBindRole.role.name == "DRIVER") userWithRole.driverIsChecked = true;
+          if (userBindRole.role.name == "USER") userWithRole.userIsChecked = true;
         });
         userWithRole.username = user.username;
         userWithRole.firstname = user.firstname;
@@ -46,11 +104,11 @@ export class UserSettingsComponent implements OnInit  {
     let bool = await firstValueFrom(this.userService.isUserKathy(user.username));
     if (bool)
       await this.router.navigate(['kathy_users_order/'],
-      {
-        queryParams: {
-          param1: user.username
-        }
-      });
+        {
+          queryParams: {
+            param1: user.username
+          }
+        });
   }
 
   userOrdered(user: UserWithRoleDTO) {
@@ -60,5 +118,7 @@ export class UserSettingsComponent implements OnInit  {
           param1: user.username
         }
       });
+
+
   }
 }
