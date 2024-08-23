632cb5ca9656f166308df98d1fa343320fc331a6
Bernhard Messerli <bernhard.messerli.5@gmail.com>
2024-08-23 07:59:32 +0200

    23082024/0759

--- src/main/java/com/messerli/balmburren/controllers/AdminController.java
+++ src/main/java/com/messerli/balmburren/controllers/AdminController.java
@@ -22,7 +22,7 @@ public class AdminController {
     }
     @CrossOrigin( allowCredentials = "true")
     @PostMapping("/create_admin")
-    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     public ResponseEntity<?> createAdministrator(@RequestBody String username) {
         boolean var = userService.createAdministrator(username);
 
@@ -31,7 +31,7 @@ public class AdminController {
 
     @CrossOrigin( allowCredentials = "true")
     @PutMapping("/create/driver")
-    @PreAuthorize("hasAnyAuthority('DRIVER')")
+    @PreAuthorize("hasAnyAuthority('DRIVER','ADMIN','SUPER_ADMIN')")
     public ResponseEntity<?> createDriver(@RequestBody String username) {
         boolean var = userService.createDriver(username);
 
@@ -49,38 +49,44 @@ public class AdminController {
 
     @CrossOrigin( allowCredentials = "true")
     @PutMapping("/update/user")
-    @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
-//    @PreAuthorize("isAuthenticated()")
+    @PreAuthorize("hasAnyAuthority('USER','ADMIN','SUPER_ADMIN')")
+//    @PreAuthorize("isAuthenticated()")'SUPER_ADMIN'
     public ResponseEntity<Optional<User>> updateUser(@RequestBody User user) {
         return ResponseEntity.ok().body(userService.updateUser(user));
     }
 
     @CrossOrigin( allowCredentials = "true")
     @GetMapping("/is_admin/{username}")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     public ResponseEntity<Boolean> isAdmin(@PathVariable("username") String username) {
         return ResponseEntity.ok().body(userService.isAdmin(username));
     }
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('USER','ADMIN','SUPER_ADMIN')")
     @GetMapping("/is_basic/{username}")
     public ResponseEntity<Boolean> isBasic(@PathVariable("username") String username) {
         return ResponseEntity.ok().body(userService.isBasic(username));
     }
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('USER_KATHY','ADMIN','SUPER_ADMIN')")
     @GetMapping("/is_user_kathy/{username}")
     public ResponseEntity<Boolean> isUserKathy(@PathVariable("username") String username) {
         return ResponseEntity.ok().body(userService.isUserKathy(username));
     }
 
     @CrossOrigin( allowCredentials = "true")
+
     @GetMapping("/is_driver/{username}")
+    @PreAuthorize("hasAnyAuthority('DRIVER','ADMIN','SUPER_ADMIN')")
     public ResponseEntity<Boolean> isDriver(@PathVariable("username") String username) {
         return ResponseEntity.ok().body(userService.isDriver(username));
     }
 
     @CrossOrigin( allowCredentials = "true")
     @GetMapping("is_kathy/{username}")
+    @PreAuthorize("hasAnyAuthority('KATHY','ADMIN','SUPER_ADMIN')")
     public ResponseEntity<Boolean> isKathy(@PathVariable("username") String username) {
         return ResponseEntity.ok().body(userService.isKathy(username));
     }
--- src/main/java/com/messerli/balmburren/controllers/AuthenticationController.java
+++ src/main/java/com/messerli/balmburren/controllers/AuthenticationController.java
@@ -16,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
