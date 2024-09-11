8e92aea6dd33fc4c36d41570a71826252f35f403
Bernhard Messerli <bernhard.messerli.5@gmail.com>
2024-09-11 07:41:29 +0200

    11092024/0741

--- frontend/src/main/angularclient/src/app/admin/components/driver/driver-work/driver-work.component.ts
+++ frontend/src/main/angularclient/src/app/admin/components/driver/driver-work/driver-work.component.ts
@@ -2,9 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {UserDTO} from "../../../../components/user/service/userDTO";
 import {UserWithRoleDTO} from "../../../../components/user/service/userWithRoleDTO";
 import {UserService} from "../../../../components/user/service/user-service.service";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {firstValueFrom} from "rxjs";
 import {UserBindRoleDTO} from "../../../../components/user/service/userBindRoleDTO";
+import {DatesDTO} from "../../tour/service/DatesDTO";
+import {WorkDTO} from "../../tour/service/workDTO";
+import {TourServiceService} from "../../tour/service/tour-service.service";
 
 @Component({
   selector: 'app-driver-work',
@@ -12,69 +15,106 @@ import {UserBindRoleDTO} from "../../../../components/user/service/userBindRoleD
   styleUrls: ['./driver-work.component.css']
 })
 export class DriverWorkComponent implements OnInit {
-  users: UserDTO[] = [];
-  usersWithRole: UserWithRoleDTO[] = [];
+  dates: DatesDTO = new DatesDTO();
+  work: WorkDTO = new WorkDTO();
+  user: UserDTO = new UserDTO();
+  counter: number = 0;
+  success: string = "";
+  error: string = "";
+  param1: string | null = "";
 
   constructor(private userService: UserService,
-              private router: Router) {
+              private tourService: TourServiceService,
+              private router: Router,
+              private route: ActivatedRoute) {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => {
+      return false;}
   }
 
   async ngOnInit(){
-    this.users = await firstValueFrom(this.userService.findAll());
-    if(this.users) {
-      this.users = this.users.sort((u1: UserDTO, u2: UserDTO) => {
-        if (u1.lastname && u2.lastname) return u1.lastname.localeCompare(u2.lastname)
-        else return 0;
-      });
-      for (const user of this.users) {
-        let user1: UserDTO = await firstValueFrom(this.userService.findUser(user.username));
-        let userWithRole: UserWithRoleDTO = new UserWithRoleDTO();
-        // let userBindRoles: UserBindRoleDTO[] = await firstValueFrom(this.userService.findAllRolesForPerson(user.username));
-        let userBindRoles: UserBindRoleDTO[] = [];
-        user1.roles.forEach(e => {
-          let userBindRole: UserBindRoleDTO = new UserBindRoleDTO();
-          userBindRole.role = e;
-          userBindRole.person = user1;
-          userBindRoles.push(userBindRole);
-        })
+    this.param1= this.route.snapshot.queryParamMap.get('param1');
+    if (this.param1 != null) this.user = await firstValueFrom(this.userService.findUser(this.param1));
+  }
 
-        userBindRoles.forEach(userBindRole => {
-          if (userBindRole.role.name == "ADMIN") userWithRole.adminIsChecked = true;
-          if (userBindRole.role.name == "DRIVER") userWithRole.driverIsChecked = true;
-          if (userBindRole.role.name == "USER") userWithRole.userIsChecked = true;
-        });
-        userWithRole.username = user.username;
-        userWithRole.firstname = user.firstname;
-        userWithRole.lastname = user.lastname;
-        this.usersWithRole.push(userWithRole);
+  async showWork() {
+    this.error = "";
+    this.success = "";
+    this.work = new WorkDTO();
+    this.dates.date = new Date(this.dates.date).toISOString().split('T')[0];
+    this.dates = await firstValueFrom(this.tourService.createDates(this.dates));
+    let user = await firstValueFrom(this.userService.currentUser());
+    this.user = await firstValueFrom(this.userService.findUser(user.username));
+    if(this.compare(new Date(this.dates.date))){
+      console.log("Id Dates: " + this.dates.id);
+      let work = await firstValueFrom(this.tourService.getWork(this.user.username, this.dates));
+      if (work) {
+        this.work = work;
+        // this.success = "Arbeit wurde gespeichert!"
       }
+      else return;
+    } else this.error ="Datum liegt in der Vergangenheit: Keine Berechtigung!"
+  }
+
+  async apply() {
+    this.work.date = this.dates;
+    this.work.user = this.user;
+    if (this.work.id != "") {
+      try {
+        this.work = await firstValueFrom(this.tourService.putWork(this.work));
+      } catch (error: any) {
+        if (error.status != 200) this.error= "Speichern hat nicht funktioniert!"
+        await this.router.navigate(['/work']);
+        return;
+      }
+      this.success = "Speichern hat funktioniert!"
+      return;
     }
+    else {
+      try{
+        this.work = await firstValueFrom(this.tourService.createWork(this.work));
+      }catch (error: any) {
+        if (error.status != 200) this.error= "Speichern hat nicht funktioniert!"
+        await this.router.navigate(['/work']);
+        return;
+      }
+      this.success = "Speichern hat funktioniert!"
+      return;
+    }
+
   }
 
-  driverWork(user: UserDTO) {
-    this.router.navigate(['admin_users_settings/'],
-      {
-        queryParams: {
-          param1: user.username
-        }
-      });
+  async clear() {
+    if (this.counter == 6) {
+      await firstValueFrom(this.tourService.deleteWork(this.user.username, this.dates));
+      this.counter = 0;
+      this.success = "Arbeit wurde gelöscht!"
+      await this.router.navigate(['/work']);
+      return;
+    }
+    this.counter ++;
+    let c = 7 - this.counter;
+    this.error = "Tippe " + c +  " mal zum Löschen!";
   }
 
-  userOrder(user: UserWithRoleDTO) {
-    this.router.navigate(['admin_users_order/'],
-      {
-        queryParams: {
-          param1: user.username
-        }
-      });
+  compare(date: Date): boolean {
+    let now1 = new Date().toISOString().split('T')[0]
+    date = new Date(date);
+    return date.toISOString().split('T')[0] >= now1;
   }
 
-  userOrdered(user: UserWithRoleDTO) {
-    this.router.navigate(['admin_users_ordered/'],
-      {
-        queryParams: {
-          param1: user.username
-        }
-      });
+  computeWorktime(){
+    let total: number = 0;
+    let arr = this.work.endTime.split(':');
+    let arr2 = this.work.startTime.split(':');
+    // console.log("arr: " + arr[0] + '/ ' + arr[1]);
+    // console.log("arr2: " + arr2[0] + '/ ' + arr2[1]);
+    total = parseInt(arr[0]) - parseInt(arr2[0]);
+    // console.log("total: " + total);
+    total = total + (parseFloat(arr[1]) - parseFloat(arr2[1]))/60;
+    let str = total.toFixed(2);
+    // console.log("startZeit: " + this.work.startTime);
+    // console.log("Total: " + str);
+    this.work.workTime = str;
   }
 }
+
