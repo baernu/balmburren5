87039bfd4808a4f4a52867ed3754056459449f40
Bernhard Messerli <bernhard.messerli.5@gmail.com>
2024-09-15 10:24:39 +0200

    15092024/1024

--- src/main/java/com/messerli/balmburren/controllers/UserBindController.java
+++ src/main/java/com/messerli/balmburren/controllers/UserBindController.java
@@ -10,6 +10,7 @@ import com.messerli.balmburren.services.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -32,12 +33,14 @@ public class UserBindController {
     private DatesService datesService;
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("address/")
     ResponseEntity<Optional<Address>> createAddress(@RequestBody Address address) {
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/address").toUriString());
         return ResponseEntity.created(uri).body(userBindService.saveAddress(address));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @PutMapping("address/")
     ResponseEntity<Optional<Address>> putAddress(@RequestBody Address address) {
         Optional<Address> address1 = userBindService.getAddress(address.getId());
@@ -45,6 +48,7 @@ public class UserBindController {
         return ResponseEntity.ok().body(userBindService.putAddress(address));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     @DeleteMapping("address/{id}")
     ResponseEntity<Optional<Address>> deleteAddress(@PathVariable("id") Long id) {
         Optional<Address> address1 = userBindService.getAddress(id);
@@ -53,6 +57,7 @@ public class UserBindController {
 
     @CrossOrigin( allowCredentials = "true")
     @GetMapping("address/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     ResponseEntity<Optional<Address>> getAddress(@PathVariable("id") Long id) {
         Optional<Address> address = userBindService.getAddress(id);
         if (!address.isPresent()) throw new NoSuchElementFoundException("Address not found");
@@ -62,12 +67,14 @@ public class UserBindController {
 
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("person/bind/deliveraddress/")
     ResponseEntity<Optional<PersonBindDeliverAddress>> createPersonBindDeliverAddress(@RequestBody PersonBindDeliverAddress personBindDeliverAddress) {
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/person/bind/deliveraddress").toUriString());
         return ResponseEntity.created(uri).body(userBindService.savePersonBindDeliverAddress(personBindDeliverAddress));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @PutMapping("person/bind/deliveraddress/")
     ResponseEntity<Optional<PersonBindDeliverAddress>> putPersonBindDeliverAddress(@RequestBody PersonBindDeliverAddress personBindDeliverAddress) {
         Optional<PersonBindDeliverAddress> personBindDeliverAddress1 = userBindService.getPersonBindDeliverAddress(personBindDeliverAddress.getUser());
@@ -75,6 +82,7 @@ public class UserBindController {
         return ResponseEntity.ok().body(userBindService.putPersonBindDeliverAddress(personBindDeliverAddress));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("person/bind/deliveraddress/{username}")
     ResponseEntity<Optional<PersonBindDeliverAddress>> getPersonBindDeliverAddress(@PathVariable("username") String username) {
         Optional<PersonBindDeliverAddress> personBindDeliverAddress1 = userBindService.getPersonBindDeliverAddress(getPeople(username).get());
@@ -82,12 +90,14 @@ public class UserBindController {
         return ResponseEntity.ok().body(personBindDeliverAddress1);}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("person/bind/deliveraddress/exist/{username}")
     ResponseEntity<Boolean> existPersonBindDeliverAddress(@PathVariable("username") String username) {
         boolean bool = userBindService.existPersonBindDeliverAddress(getPeople(username).get());
         return ResponseEntity.ok().body(bool);}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     @DeleteMapping("person/bind/deliveraddress/{people}")
     ResponseEntity<Optional<PersonBindDeliverAddress>> deletePersonBindDeliverAddress(@PathVariable("username") String username) {
         Optional<PersonBindDeliverAddress> personBindDeliverAddress1 = userBindService.getPersonBindDeliverAddress(getPeople(username).get());
@@ -95,17 +105,20 @@ public class UserBindController {
         return ResponseEntity.ok().body(userBindService.deletePersonBindDeliverAddress(personBindDeliverAddress1.get().getUser()));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("person/bind/invoice/")
     ResponseEntity<Optional<PersonBindInvoice>> createPersonBindInvoice(@RequestBody PersonBindInvoice personBindInvoice) {
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/person/bind/invoice").toUriString());
         return ResponseEntity.created(uri).body(userBindService.savePersonBindInvoice(personBindInvoice));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @PutMapping("person/bind/invoice/")
     ResponseEntity<Optional<PersonBindInvoice>> putPersonBindInvoice(@RequestBody PersonBindInvoice personBindInvoice) {
         return ResponseEntity.ok().body(userBindService.putPersonBindInvoice(personBindInvoice));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("person/bind/invoice/{dateFrom}/{dateTo}/{invoice}/{deliver}")
     ResponseEntity<Optional<PersonBindInvoice>> getPersonBindInvoice(@PathVariable("dateFrom") Long dateFrom, @PathVariable("dateTo") Long dateTo,
                                                            @PathVariable("invoice") String invoice, @PathVariable("deliver") String deliver) {
@@ -114,6 +127,7 @@ public class UserBindController {
         return ResponseEntity.ok().body(personBindInvoice1);}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("person/bind/invoice/exist/{dateFrom}/{dateTo}/{invoice}/{deliver}")
     ResponseEntity<Boolean> existPersonBindInvoice(@PathVariable("dateFrom") Long dateFrom, @PathVariable("dateTo") Long dateTo,
                                                            @PathVariable("invoice") String invoice, @PathVariable("deliver") String deliver) {
@@ -121,6 +135,7 @@ public class UserBindController {
         return ResponseEntity.ok().body(bool);}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     @DeleteMapping("person/bind/invoice/{dateFrom}/{dateTo}/{invoice}/{deliver}")
     ResponseEntity<Optional<PersonBindInvoice>> deletePersonBindInvoice(@PathVariable("dateFrom") Long dateFrom, @PathVariable("dateTo") Long dateTo,
                                                               @PathVariable("invoice") String invoice, @PathVariable("deliver") String deliver) {
@@ -129,22 +144,26 @@ public class UserBindController {
         return ResponseEntity.ok().body(userBindService.deletePersonBindInvoice(getDates(dateFrom), getDates(dateTo), getPeople(invoice).get(), getPeople(deliver).get()));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     @PatchMapping("person/bind/invoice/")
     ResponseEntity<Optional<PersonBindInvoice>> deletePersonBindInvoiceById(@RequestBody PersonBindInvoice personBindInvoice) {
     userBindService.deletePersonBindInvoiceById(personBindInvoice);
         return ResponseEntity.ok().body(Optional.ofNullable(personBindInvoice));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("person/bind/invoice/invoice/{username}")
     ResponseEntity<Optional<List<PersonBindInvoice>>> getAllPersonBindInvoiceForInvoice(@PathVariable("username") String username) {
         return ResponseEntity.ok().body(userBindService.getAllPersonBindInvoiceForInvoice(getPeople(username).get()));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     @GetMapping("person/bind/invoice/")
     ResponseEntity<Optional<List<PersonBindInvoice>>> getAllPersonBindInvoice() {
         return ResponseEntity.ok().body(userBindService.getAllPersonBindInvoice());}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     @GetMapping("person/bind/invoice/{dateFrom}")
     ResponseEntity<Optional<List<PersonBindInvoice>>> getAllPersonBindInvoiceDateFrom(@PathVariable("dateFrom") String dateFrom) {
         return ResponseEntity.ok().body(userBindService.getAllPersonBindInvoiceForDateFrom(dateFrom));}
@@ -152,17 +171,20 @@ public class UserBindController {
 
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN', 'DRIVER')")
     @PostMapping("driver/bind/invoice/")
     ResponseEntity<Optional<DriverBindInvoice>> createDriverBindInvoice(@RequestBody DriverBindInvoice driverBindInvoice) {
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/driver/bind/invoice").toUriString());
         return ResponseEntity.created(uri).body(userBindService.saveDriverBindInvoice(driverBindInvoice));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN', 'DRIVER')")
     @PutMapping("driver/bind/invoice/")
     ResponseEntity<Optional<DriverBindInvoice>> putDriverBindInvoice(@RequestBody DriverBindInvoice driverBindInvoice) {
         return ResponseEntity.ok().body(userBindService.putDriverBindInvoice(driverBindInvoice));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN', 'DRIVER')")
     @GetMapping("driver/bind/invoice/{dateFrom}/{dateTo}/{invoice}")
     ResponseEntity<Optional<DriverBindInvoice>> getDriverBindInvoice(@PathVariable("dateFrom") Long dateFrom, @PathVariable("dateTo") Long dateTo,
                                                                      @PathVariable("invoice") String invoice) {
@@ -171,6 +193,7 @@ public class UserBindController {
         return ResponseEntity.ok().body(driverBindInvoice1);}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN', 'DRIVER')")
     @GetMapping("driver/bind/invoice/exist/{dateFrom}/{dateTo}/{invoice}")
     ResponseEntity<Boolean> existDriverBindInvoice(@PathVariable("dateFrom") Long dateFrom, @PathVariable("dateTo") Long dateTo,
                                                    @PathVariable("invoice") String invoice) {
@@ -178,6 +201,7 @@ public class UserBindController {
         return ResponseEntity.ok().body(bool);}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     @DeleteMapping("driver/bind/invoice/{dateFrom}/{dateTo}/{invoice}")
     ResponseEntity<Optional<DriverBindInvoice>> deleteDriverBindInvoice(@PathVariable("dateFrom") Long dateFrom, @PathVariable("dateTo") Long dateTo,
                                                                         @PathVariable("invoice") String invoice) {
@@ -186,6 +210,7 @@ public class UserBindController {
         return ResponseEntity.ok().body(userBindService.deleteDriverBindInvoice(getDates(dateFrom), getDates(dateTo), getPeople(invoice).get()));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     @PatchMapping("driver/bind/invoice/")
     ResponseEntity<Optional<DriverBindInvoice>> deleteDriverBindInvoiceById(@RequestBody DriverBindInvoice driverBindInvoice) {
         userBindService.deleteDriverBindInvoiceById(driverBindInvoice);
@@ -193,32 +218,38 @@ public class UserBindController {
 
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("person/bind/invoice/deliver/{username}")
     ResponseEntity<Optional<List<PersonBindInvoice>>> getAllPersonBindInvoiceForDeliver(@PathVariable("username") String username) {
         return ResponseEntity.ok().body(userBindService.getAllPersonBindInvoiceForDeliver(getPeople(username).get()));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN', 'DRIVER')")
     @GetMapping("driver/bind/invoice/invoice/{username}")
     ResponseEntity<Optional<List<DriverBindInvoice>>> getAllDriverBindInvoiceForInvoice(@PathVariable("username") String username) {
         return ResponseEntity.ok().body(userBindService.getAllDriverBindInvoiceForInvoice(getPeople(username).get()));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     @GetMapping("driver/bind/invoice/")
     ResponseEntity<Optional<List<DriverBindInvoice>>> getAllDriverBindInvoice() {
         return ResponseEntity.ok().body(userBindService.getAllDriverBindInvoice());}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     @GetMapping("driver/bind/invoice/{dateFrom}")
     ResponseEntity<Optional<List<DriverBindInvoice>>> getAllDriverBindInvoiceDateFrom(@PathVariable("dateFrom") String dateFrom) {
         return ResponseEntity.ok().body(userBindService.getAllDriverBindInvoiceForDateFrom(dateFrom));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("person/bind/phone/")
     ResponseEntity<Optional<PersonBindPhone>> createPersonBindPhone(@RequestBody PersonBindPhone personBindPhone) {
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/person/bind/phone").toUriString());
         return ResponseEntity.created(uri).body(userBindService.savePersonBindPhone(personBindPhone));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @PutMapping("person/bind/phone/")
     ResponseEntity<Optional<PersonBindPhone>> putPersonBindPhone(@RequestBody PersonBindPhone personBindPhone) {
         Optional<PersonBindPhone> personBindPhone1 = userBindService.getPersonBindPhone(personBindPhone.getUser());
@@ -226,6 +257,7 @@ public class UserBindController {
         return ResponseEntity.ok().body(userBindService.putPersonBindPhone(personBindPhone));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("person/bind/phone/{username}")
     ResponseEntity<Optional<PersonBindPhone>> getPersonBindPhone(@PathVariable("username") String username) {
         Optional<PersonBindPhone> personBindPhone = userBindService.getPersonBindPhone(getPeople(username).get());
@@ -233,12 +265,14 @@ public class UserBindController {
         return ResponseEntity.ok().body(personBindPhone);}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("person/bind/phone/exist/{username}")
     ResponseEntity<Boolean> existPersonBindPhone(@PathVariable("username") String username) {
         boolean bool = userBindService.existPersonBindPhone(getPeople(username).get());
         return ResponseEntity.ok().body(bool);}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     @DeleteMapping("person/bind/phone/{username}")
     ResponseEntity<Optional<PersonBindPhone>> deletePersonBindPhone(@PathVariable("username") String username) {
         Optional<PersonBindPhone> personBindPhone = userBindService.getPersonBindPhone(getPeople(username).get());
@@ -266,23 +300,27 @@ public class UserBindController {
     }
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("person/bind/tour/{username}/{tour}")
     ResponseEntity<Optional<PersonBindTour>> getPersonBindTour(@PathVariable("tour") String tour, @PathVariable("username") String username) {
         Optional<PersonBindTour> personBindTour = userBindService.getPersonBindTour(getPeople(username).get(), getTour(tour).get());
         return ResponseEntity.ok().body(personBindTour);}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     @PostMapping("person/bind/tour/")
     ResponseEntity<Optional<PersonBindTour>> createPersonBindTour(@RequestBody PersonBindTour personBindTour) {
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/person/bind/tour").toUriString());
         return ResponseEntity.created(uri).body(userBindService.savePersonBindTour(personBindTour));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     @PutMapping("person/bind/tour/")
     ResponseEntity<Optional<PersonBindTour>> updatePersonBindTour(@RequestBody PersonBindTour personBindTour) {
         return ResponseEntity.ok().body(userBindService.savePersonBindTour(personBindTour));}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN')")
     @DeleteMapping("person/bind/tour/{username}/{tour}")
     ResponseEntity<Optional<PersonBindTour>> deletePersonBindTour(@PathVariable("tour") String tour, @PathVariable("username") String username) {
         Optional<PersonBindTour> personBindTour = userBindService.getPersonBindTour(getPeople(username).get(), getTour(tour).get());
@@ -290,6 +328,7 @@ public class UserBindController {
         return ResponseEntity.ok().body(personBindTour);}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPER_ADMIN', 'DRIVER')")
     @GetMapping("person/bind/tour/{tour}")
     ResponseEntity<Optional<List<PersonBindTour>>> getAllPersonsForTour(@PathVariable("tour") String tour) {
         if (tourService.existTour(tour)) {
@@ -298,6 +337,7 @@ public class UserBindController {
         else { throw new NoSuchElementFoundException("Tour not found"); }}
 
     @CrossOrigin( allowCredentials = "true")
+    @PreAuthorize("isAuthenticated()")
     @GetMapping ("person/bind/tour/exist/{username}/{tour}")
     ResponseEntity<Boolean> existTourForPerson(@PathVariable("username") String username, @PathVariable("tour") String tour) {
         boolean bool = userBindService.existPersonAndTour(getPeople(username).get(), getTour(tour).get());
