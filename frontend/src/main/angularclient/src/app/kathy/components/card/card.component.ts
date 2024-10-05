f1055f1052b5b4e7e1356df1030c28ba37c36381
Bernhard Messerli <bernhard.messerli.5@gmail.com>
2024-10-05 19:25:55 +0200

    05102024/1925

--- frontend/src/main/angularclient/src/app/kathy/components/card/card.component.ts
+++ frontend/src/main/angularclient/src/app/kathy/components/card/card.component.ts
@@ -15,7 +15,42 @@ export class CardComponent {
   }
 
   async uploadFile(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];  // Get the first file (JPEG file in this case)
+
+      const base64String = await this.convertArrayBufferToBase64(file);
+      this.card.base64 = base64String;
+
+    }
   }
 
+  private convertArrayBufferToBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.readAsArrayBuffer(file);
+
+      reader.onload = () => {
+        const arrayBuffer = reader.result as ArrayBuffer;
+        const base64String = this.arrayBufferToBase64(arrayBuffer);
+        resolve(base64String);
+      };
+
+      reader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  }
+
+  private arrayBufferToBase64(buffer: ArrayBuffer): string {
+    const bytes = new Uint8Array(buffer);
+    let binary = '';
+    for (let i = 0; i < bytes.byteLength; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    return btoa(binary);  // Convert binary string to base64
+  }
 
 }
