3539819e2318401e416dbef1bf3cfe84bee7ad69
Bernhard Messerli <bernhard.messerli.5@gmail.com>
2024-10-06 11:19:06 +0200

    06102024/1119

--- frontend/src/main/angularclient/src/app/links/actual/actual.component.ts
+++ frontend/src/main/angularclient/src/app/links/actual/actual.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {CardDTO} from "../../admin/components/product/service/cardDTO";
+import {ProductService} from "../../admin/components/product/service/product.service";
+import {Router} from "@angular/router";
+import {firstValueFrom} from "rxjs";
 
 @Component({
   selector: 'app-actual',
@@ -6,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./actual.component.css']
 })
 export class ActualComponent {
+  cards: CardDTO[] = []
 
+  constructor(private productService: ProductService,
+              private router: Router) {
+  }
+  async ngOnInit() {
+    this.cards = await firstValueFrom(this.productService.findAllActiveCards(true));
+  }
 }
