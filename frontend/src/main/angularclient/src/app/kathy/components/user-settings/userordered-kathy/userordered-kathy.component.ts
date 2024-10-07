e5aeb67e4be976883470f957526eff17a8bdaddb
Bernhard Messerli <bernhard.messerli.5@gmail.com>
2024-10-07 09:54:00 +0200

    07102024/0953

--- frontend/src/main/angularclient/src/app/kathy/components/user-settings/userordered-kathy/userordered-kathy.component.ts
+++ frontend/src/main/angularclient/src/app/kathy/components/user-settings/userordered-kathy/userordered-kathy.component.ts
@@ -6,6 +6,7 @@ import {TourServiceService} from "../../../../admin/components/tour/service/tour
 import {UserService} from "../../../../components/user/service/user-service.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {firstValueFrom} from "rxjs";
+import {groupebyDTO} from "../../../../components/user/service/groupbyDTO";
 
 @Component({
   selector: 'app-userordered-kathy',
@@ -13,13 +14,51 @@ import {firstValueFrom} from "rxjs";
   styleUrls: ['./userordered-kathy.component.css']
 })
 export class UserorderedKathyComponent {
+//   param1: string | null = "";
+//   orders: OrderDTO[] = [];
+//   username: string = "";
+//   people: UserDTO = new UserDTO();
+//   dateFrom: DatesDTO = new DatesDTO();
+//   dateTo: DatesDTO = new DatesDTO();
+//   price: number = 0;
+//
+//   constructor(
+//     private tourService: TourServiceService,
+//     private userService: UserService,
+//     private route: ActivatedRoute,
+//     private router: Router){
+//     this.router.routeReuseStrategy.shouldReuseRoute = () => {
+//       return false;
+//     };
+//   }
+//
+//   async ngOnInit(): Promise<void> {
+//     this.param1 = this.route.snapshot.queryParamMap.get('param1');
+//     if (this.param1 != null) this.people = await firstValueFrom(this.userService.findUser(this.param1));
+//   }
+//
+//   async apply() {
+//     this.dateFrom.date = new Date(this.dateFrom.date).toISOString().split('T')[0];
+//     this.dateFrom = await firstValueFrom(this.tourService.createDates(this.dateFrom));
+//     this.dateTo.date = new Date(this.dateTo.date).toISOString().split('T')[0];
+//     this.dateTo = await firstValueFrom(this.tourService.createDates(this.dateTo));
+//     this.orders = await firstValueFrom(this.userService.getAllOrderForPersonBetween(this.dateFrom, this.dateTo, this.people));
+//     this.orders = this.orders.filter(order => order.quantityDelivered > 0);
+//     this.orders.sort((t1: OrderDTO, t2: OrderDTO) => t1.productBindInfos.product.name.localeCompare(t2.productBindInfos.product.name));
+//     this.orders.sort((t1: OrderDTO, t2: OrderDTO) => t1.productBindInfos.productDetails.category.localeCompare(t2.productBindInfos.productDetails.category));
+//     this.orders.sort((t1: OrderDTO, t2: OrderDTO) => t1.date.date.localeCompare(t2.date.date));
+//     this.orders.forEach(o => this.price += o.productBindInfos.productDetails.price * o.quantityDelivered);
+//   }
+// }
+
   param1: string | null = "";
   orders: OrderDTO[] = [];
   username: string = "";
-  people: UserDTO = new UserDTO();
+  user: UserDTO = new UserDTO();
   dateFrom: DatesDTO = new DatesDTO();
   dateTo: DatesDTO = new DatesDTO();
   price: number = 0;
+  categories: groupebyDTO[] = [];
 
   constructor(
     private tourService: TourServiceService,
@@ -33,7 +72,7 @@ export class UserorderedKathyComponent {
 
   async ngOnInit(): Promise<void> {
     this.param1 = this.route.snapshot.queryParamMap.get('param1');
-    if (this.param1 != null) this.people = await firstValueFrom(this.userService.findUser(this.param1));
+    if (this.param1 != null) this.user = await firstValueFrom(this.userService.findUser(this.param1));
   }
 
   async apply() {
@@ -41,11 +80,30 @@ export class UserorderedKathyComponent {
     this.dateFrom = await firstValueFrom(this.tourService.createDates(this.dateFrom));
     this.dateTo.date = new Date(this.dateTo.date).toISOString().split('T')[0];
     this.dateTo = await firstValueFrom(this.tourService.createDates(this.dateTo));
-    this.orders = await firstValueFrom(this.userService.getAllOrderForPersonBetween(this.dateFrom, this.dateTo, this.people));
+    this.orders = await firstValueFrom(this.userService.getAllOrderForPersonBetween(this.dateFrom, this.dateTo, this.user));
     this.orders = this.orders.filter(order => order.quantityDelivered > 0);
     this.orders.sort((t1: OrderDTO, t2: OrderDTO) => t1.productBindInfos.product.name.localeCompare(t2.productBindInfos.product.name));
     this.orders.sort((t1: OrderDTO, t2: OrderDTO) => t1.productBindInfos.productDetails.category.localeCompare(t2.productBindInfos.productDetails.category));
     this.orders.sort((t1: OrderDTO, t2: OrderDTO) => t1.date.date.localeCompare(t2.date.date));
     this.orders.forEach(o => this.price += o.productBindInfos.productDetails.price * o.quantityDelivered);
+
+    this.showGroup();
+  }
+
+  showGroup() {
+    const group = this.orders.reduce((acc: any, curr) => {
+      let key = curr.date.date;
+      if (!acc[key]) {
+        acc[key] = [];
+      }
+      acc[key].push(curr);
+      return acc;
+    }, {});
+
+    //Get the categories and product related.
+    this.categories = Object.keys(group).map(key => ({
+      category: key,
+      products: group[key],
+    }));
   }
 }
