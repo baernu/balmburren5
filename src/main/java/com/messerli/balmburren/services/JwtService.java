f66cfc60d95375e27bf72dffc981e9e52a5b9ad0
Bernhard Messerli <bernhard.messerli.5@gmail.com>
2025-01-09 20:50:25 +0100

    09012025/2050

--- src/main/java/com/messerli/balmburren/services/JwtService.java
+++ src/main/java/com/messerli/balmburren/services/JwtService.java
@@ -57,7 +57,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
